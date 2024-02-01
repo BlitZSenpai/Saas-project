433d45b8e4f19b4fca5c5323a75c2380592f00f7
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2024-02-01 23:42:16 +0530

    add: site components and layout

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <>
@@ -6,6 +8,16 @@ export default function Home() {
         <p className="text-center">Run your agency, in one place.</p>
         <div className="bg-gradient-to-r from-primary to-secondary-foreground text-transparent bg-clip-text relative">
           <h1 className="text-9xl font-bold text-center md:text-[300px]">Cryo</h1>
+        </div>
+        <div className="flex justify-center items-center relative md:mt-[-70px]">
+          <Image
+            src="/assets/preview.png"
+            alt="Preview image"
+            height={1200}
+            width={1200}
+            className="rounded-t-2xl border-2 border-muted"
+          />
+          <div className="bottom-0 top-[50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10"></div>
         </div>
       </section>
     </>
