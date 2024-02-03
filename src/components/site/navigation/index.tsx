7eba32d106e106f8262aaf0e169d2d78d818e85d
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2024-02-03 12:19:11 +0530

    fix: navbar style fixes

--- src/components/site/navigation/index.tsx
+++ src/components/site/navigation/index.tsx
@@ -5,15 +5,15 @@ import Image from "next/image";
 import Link from "next/link";
 
 type NavigationProps = {
-  user?: User | null;
+  user?: null | User;
 };
 
 const Navigation = ({ user }: NavigationProps) => {
   return (
-    <div className="p-4 flex items-center justify-between relative">
+    <div className="fixed top-0 right-0 left-0 p-4 flex items-center justify-between z-10">
       <aside className="flex items-center gap-2">
-        <Image src="/assets/logo.svg" alt="logo" height={40} width={40} />
-        <span className="text-xl font-bold">Cryo.</span>
+        <Image src={"./assets/logo.svg"} width={40} height={40} alt="logo" />
+        <span className="text-xl font-bold"> Cryo.</span>
       </aside>
       <nav className="hidden md:block absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%]">
         <ul className="flex items-center justify-center gap-8">
@@ -24,7 +24,7 @@ const Navigation = ({ user }: NavigationProps) => {
         </ul>
       </nav>
       <aside className="flex gap-2 items-center">
-        <Link href="/agency" className="bg-primary text-white p-2 px-4 rounded-md hover:bg-primary/80">
+        <Link href={"/agency"} className="bg-primary text-white p-2 px-4 rounded-md hover:bg-primary/80">
           Login
         </Link>
         <UserButton />
