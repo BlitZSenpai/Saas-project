12a30ac3a0da869b0046aa6af902d5514152b830
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2024-02-04 23:38:31 +0530

    upd config

---
+++ next.config.js
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "uploadthing.com",
+      },
+      {
+        hostname: "utfs.io",
+      },
+      {
+        hostname: "img.clerk.com",
+      },
+      {
+        hostname: "subdomain",
+      },
+      {
+        hostname: "files.stripe.com",
+      },
+    ],
+  },
+  reactStrictMode: false,
+};
+
+module.exports = nextConfig;
