b0f33c1214ba1c68bb48fb0d3e95f178d60b49d4
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2024-02-08 22:54:18 +0530

    add: createTeam user and veriftandacceptinvitation queries

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -2,11 +2,13 @@
 
 import { currentUser } from "@clerk/nextjs";
 import { db } from "./db";
+import { redirect } from "next/navigation";
+import { User } from "@prisma/client";
 
 export const getAuthUserDetails = async () => {
   const user = await currentUser();
   if (!user) {
-    throw new Error("Not Authenticated");
+    throw new Error("Unauthorized");
   }
 
   const userData = await db.user.findUnique({
@@ -29,4 +31,36 @@ export const getAuthUserDetails = async () => {
   });
 
   return userData;
+};
+
+export const createTeamUser = async (agencyId: string, user: User) => {
+  if (user.role === "AGENCY_OWNER") return null;
+
+  const response = await db.user.create({ data: { ...user } });
+  return response;
+};
+
+export const verifyAndAcceptInvitation = async () => {
+  const user = await currentUser();
+  if (!user) redirect("/sign-in");
+
+  const invitationExists = await db.invitation.findUnique({
+    where: {
+      email: user.emailAddresses[0].emailAddress,
+      status: "PENDING",
+    },
+  });
+
+  if (invitationExists) {
+    const userDetails = await createTeamUser(invitationExists.agencyId, {
+      email: invitationExists.email,
+      agencyId: invitationExists.agencyId,
+      avatarUrl: user.imageUrl,
+      id: user.id,
+      name: `${user.firstName} ${user.lastName}`,
+      role: invitationExists.role,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+  }
 };
