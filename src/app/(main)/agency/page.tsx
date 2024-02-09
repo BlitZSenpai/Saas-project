40f51d3691726fb280c21642d366dd323cd515a1
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2024-02-09 16:18:51 +0530

    feat: agency page annd more queries

--- src/app/(main)/agency/page.tsx
+++ src/app/(main)/agency/page.tsx
@@ -1,9 +1,38 @@
+import { getAuthUserDetails, verifyAndAcceptInvitation } from "@/lib/queries";
 import { currentUser } from "@clerk/nextjs";
+import { Plan } from "@prisma/client";
 import { redirect } from "next/navigation";
 
-const AgencyPage = async () => {
-  const user = await currentUser();
-  if (!user) redirect("/sign-in");
+type AgencyPageProps = {
+  params: {
+    plan: Plan;
+    state: string;
+    code: string;
+  };
+};
+
+const AgencyPage = async ({ params }: AgencyPageProps) => {
+  const agencyId = await verifyAndAcceptInvitation();
+
+  const user = await getAuthUserDetails();
+  if (agencyId) {
+    if (user?.role === "SUBACCOUNT_USER" || user?.role === "SUBACCOUNT_GUEST") {
+      return redirect("/subaccount");
+    } else if (user?.role === "AGENCY_OWNER" || user?.role === "AGENCY_ADMIN") {
+      if (params.plan) {
+        return redirect(`/agency/${agencyId}/billing?plan=${params.plan}`);
+      }
+      if (params.state) {
+        const statePath = params.state.split("__")[0];
+        const stateAgencyId = params.state.split("___")[1];
+        if (!stateAgencyId) return <div>Unauthorised</div>;
+
+        return redirect(`/agency/${stateAgencyId}/${statePath}?code=${params.code}`);
+      } else return redirect(`/agency/${agencyId}`);
+    } else {
+      return <div>Unauthorised</div>;
+    }
+  }
 
   return (
     <div>
