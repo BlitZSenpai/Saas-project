1794f26d35b7d4726830699dae42a6aa503a7e7b
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2024-02-23 18:14:24 +0530

    feat: popover command group

--- src/components/sidebar/menu-options.tsx
+++ src/components/sidebar/menu-options.tsx
@@ -1,15 +1,17 @@
 "use client";
 
-import { AgencySidebarOption, SubAccount, SubAccountSidebarOption } from "@prisma/client";
-import { ChevronsUpDown, LucideIcon, Menu } from "lucide-react";
-import React, { useEffect, useMemo, useState } from "react";
-import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
-import { Button } from "../ui/button";
 import { cn } from "@/lib/utils";
-import { AspectRatio } from "../ui/aspect-ratio";
+import { AgencySidebarOption, SubAccount, SubAccountSidebarOption } from "@prisma/client";
+import { ChevronsUpDown, Menu } from "lucide-react";
 import Image from "next/image";
-import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { useEffect, useMemo, useState } from "react";
 import Compass from "../icons/compass";
+import { AspectRatio } from "../ui/aspect-ratio";
+import { Button } from "../ui/button";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { Sheet, SheetClose, SheetContent, SheetTrigger } from "../ui/sheet";
+import { CommandInput, Command, CommandList, CommandEmpty, CommandGroup, CommandItem } from "../ui/command";
+import Link from "next/link";
 
 type MenuOptionsProps = {
   defaultOpen?: boolean;
@@ -73,7 +75,98 @@ const MenuOptions = ({
                 </div>
               </Button>
             </PopoverTrigger>
-            <PopoverContent className="w-80 h-80 mt-4 z-[200]"></PopoverContent>
+            <PopoverContent className="w-80 h-80 mt-4 z-[200]">
+              <Command className="rounded-lg">
+                <CommandInput placeholder="Search Accounts..." />
+                <CommandList className="pb-16">
+                  <CommandEmpty>No results found</CommandEmpty>
+                  {(user?.role === "AGENCY_OWNER" || user?.role === "AGENCY_ADMIN") && user?.Agency && (
+                    <CommandGroup heading="Agency">
+                      <CommandItem className="!bg-transparent my-2 text-primary broder-[1px] border-border p-2 rounded-md hover:!bg-muted cursor-pointer transition-all">
+                        {defaultOpen ? (
+                          <Link href={`/agency/${user?.Agency?.id}`} className="flex gap-4 w-full h-full">
+                            <div className="relative w-16">
+                              <Image
+                                src={user?.Agency?.agencyLogo}
+                                alt="Agency Logo"
+                                fill
+                                className="rounded-md object-contain"
+                              />
+                            </div>
+                            <div className="flex flex-col flex-1">
+                              {user?.Agency?.name}
+                              <span className="text-muted-foreground">{user?.Agency?.address}</span>
+                            </div>
+                          </Link>
+                        ) : (
+                          <SheetClose asChild>
+                            <Link href={`/agency/${user?.Agency?.id}`} className="flex gap-4 w-full h-full">
+                              <div className="relative w-16">
+                                <Image
+                                  src={user?.Agency?.agencyLogo}
+                                  alt="Agency Logo"
+                                  fill
+                                  className="rounded-md object-contain"
+                                />
+                              </div>
+                              <div className="flex flex-col flex-1">
+                                {user?.Agency?.name}
+                                <span className="text-muted-foreground">{user?.Agency?.address}</span>
+                              </div>
+                            </Link>
+                          </SheetClose>
+                        )}
+                      </CommandItem>
+                    </CommandGroup>
+                  )}
+                  <CommandGroup heading="Accounts">
+                    {!!subAccounts
+                      ? subAccounts.map((subaccount) => (
+                          <CommandItem key={subaccount.id}>
+                            {defaultOpen ? (
+                              <Link
+                                href={`/subaccount/${subaccount.id}`}
+                                className="flex gap-4 w-full h-full">
+                                <div className="relative w-16">
+                                  <Image
+                                    src={subaccount.subAccountLogo}
+                                    alt="subaccount Logo"
+                                    fill
+                                    className="rounded-md object-contain"
+                                  />
+                                </div>
+                                <div className="flex flex-col flex-1">
+                                  {subaccount.name}
+                                  <span className="text-muted-foreground">{subaccount.address}</span>
+                                </div>
+                              </Link>
+                            ) : (
+                              <SheetClose asChild>
+                                <Link
+                                  href={`/subaccount/${subaccount.id}`}
+                                  className="flex gap-4 w-full h-full">
+                                  <div className="relative w-16">
+                                    <Image
+                                      src={subaccount.subAccountLogo}
+                                      alt="subaccount Logo"
+                                      fill
+                                      className="rounded-md object-contain"
+                                    />
+                                  </div>
+                                  <div className="flex flex-col flex-1">
+                                    {subaccount.name}
+                                    <span className="text-muted-foreground">{subaccount.address}</span>
+                                  </div>
+                                </Link>
+                              </SheetClose>
+                            )}
+                          </CommandItem>
+                        ))
+                      : "No Accounts"}
+                  </CommandGroup>
+                </CommandList>
+              </Command>
+            </PopoverContent>
           </Popover>
         </div>
       </SheetContent>
